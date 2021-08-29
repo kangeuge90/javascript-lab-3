7de6a263afede8d54ef23ee846f06f712c02bb4a
Eugene Kang <kangeuge90@gmail.com>
2021-08-29 16:27:54 -0400

    First commit - WIP notesAndExtendedChallenges.js

---
+++ script.js
@@ -0,0 +1,93 @@
+//1.  
+let submissions = [{
+        name: "Jane",
+        score: 95,
+        date: "2020-01-24",
+        passed: true,
+    },
+    {
+        name: "Joe",
+        score: 77,
+        date: "2018-05-14",
+        passed: true,
+    },
+    {
+        name: "Jack",
+        score: 59,
+        date: "2019-07-05",
+        passed: false,
+    },
+    {
+        name: "Jill",
+        score: 88,
+        date: "2020-04-22",
+        passed: true,
+    },
+]
+
+//2.  
+function addSubmission(array, newName, newScore, newDate) {
+    const submission = {
+        name: newName,
+        score: newScore,
+        date: newDate,
+        passed: newScore >= 60
+    }
+    array.push(submission);
+    console.log(array);
+}
+
+//3.  
+function deleteSubmissionByIndex(array, index) {
+    array.splice(index, 1);
+}
+
+//4.  
+function deleteSubmissionByName(array, name) {
+    const index = array.findIndex(array => array.name == name)
+    array.splice(index, 1)
+}
+
+//5.  
+function editSubmission(array, index, score) {
+    array[index].score = score
+    array[index].passed = score >= 60 ? true : false;
+}
+
+//6.  
+function findSubmissionbyName(array, name) {
+    return array.find(array => array.name == name)
+}
+
+//7.  
+function findLowestScore(array) {
+    let minValue = array[0]
+    array.forEach(array => {
+        if (array.score < minValue) {
+            minValue = array
+        }
+    });
+    return minValue
+}
+
+//8.  
+function findAverageScore(array) {
+    let totalScore = 0
+    for (let submission of array) {
+        totalScore += submission.score
+    }
+    averageScore = ((totalScore) / (array.length))
+    return averageScore
+}
+
+//9.  
+function filterPassing(array) {
+    const passingList = array.filter(array => array.score >= 60)
+    return passingList
+}
+
+//10.  
+function filter90AndAbove(array) {
+    const highGrades = array.filter(array => array.score >= 90)
+    return highGrades
+}
